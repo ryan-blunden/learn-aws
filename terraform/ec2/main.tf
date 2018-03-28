3a81ced08a645cfb2227295d05ed7c0426596de4
Ryan Blunden <ryan.blunden@gmail.com>
2018-03-28 11:28:04 +0800

    The `user_data` now installs Docker and Docker Compose.

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -34,14 +34,25 @@ resource "aws_instance" "resource" {
   associate_public_ip_address = "${var.has_public_ip}"
 
   user_data = <<-EOF
-              # Update System
-              yum update -y
-              yum upgrade
-              yum install -y docker git make nano
+    #!/usr/bin/env bash
 
-              # Start Docker now that it is installed
-              service docker start
-              EOF
+    yum update -y
+    yum upgrade
+    yum install -y docker git make nano python36
+
+    # Add pip3 to `/usr/bin` so root/sudoers can find it.
+    curl -O https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py
+    rm get-pip.py
+    ln -s /usr/local/bin/pip3 /usr/bin/pip3
+    pip3 install pip setuptools --upgrade
+
+    # Install docker-compose, adding it to `/usr/bin` so root/sudoers can find it.
+    pip3 install docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+    service docker start
+  EOF
 
   tags {
     Name = "${var.instance_name}"
