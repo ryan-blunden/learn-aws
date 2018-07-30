b3729e63d10628a3c316b9b050e9655f1da6129a
Ryan Blunden <ryan.blunden@gmail.com>
2018-07-30 00:35:48 -0400

    Moved lambda dynamo s3 into lambda folder.

--- code-challenges/iam-role.md
+++
@@ -1,3 +0,0 @@
-# IAM Role Code Challenge
-
-Use the policy in `iam-policies/s3-bucket-path-restriction.json` and create an IAM role, that will allow an EC2 instance, to read and write to a specific folder (path prefix) but should not be able to read or write to other folders at the same level.
--- lambda/lambda-dynamo-s3-lab/lambda_function.py
+++ lambda/lambda-dynamo-s3-lab/lambda_function.py
@@ -1,6 +1,7 @@
-import boto3
 import os
 import urllib
+
+import boto3
 
 sesclient = boto3.client('ses')
 dynamoClient = boto3.client('dynamodb')
@@ -12,32 +13,33 @@ bucket = os.environ['BUCKET']
 image_base_url = os.environ['IMAGE_BASE_URL']
 recipient = os.environ['RECIPIENT']
 
+
 def lambda_handler(event, context):
     print(event)
-    
+
     for record in event['Records']:
         if record['eventName'] != 'INSERT':
             print("This is not an insert")
             return
-        
+
         item = record['dynamodb']['NewImage']
-        
+
         id = item['id']['N']
-        
+
         dynamo_item = dynamoClient.get_item(
-                TableName=table,
-                Key={
-                    'id': {
-                        'N': id
-                    }
+            TableName=table,
+            Key={
+                'id': {
+                    'N': id
                 }
-            )
-        
+            }
+        )
+
         print(dynamo_item)
-        
+
         image = dynamo_item['Item']['image']['S']
         text = dynamo_item['Item']['text']['S']
-        
+
         try:
             object = s3client.get_object(Bucket=bucket, Key=image)
             print(object)
@@ -48,20 +50,19 @@ def lambda_handler(event, context):
             imagedata = photoResponse.read()
             object = s3.Object(bucket, image)
             object.put(Body=imagedata)
-        
+
         print("Image: " + image)
         print("Text: " + text)
-        
-        
+
         subject = "Your meme of the day"
-        
+
         body_text = (text + "\r\n"
-                 "To see the image, go here: " + image_base_url + "/" + image
-                )
-                
+                            "To see the image, go here: " + image_base_url + "/" + image
+                     )
+
         # The HTML body of the email.
-        body_html = "<html><head></head><body><h1>" + text + "</h1><img src='" + image_base_url + "/" + image + "'/></body></html>" 
-        
+        body_html = "<html><head></head><body><h1>" + text + "</h1><img src='" + image_base_url + "/" + image + "'/></body></html>"
+
         response = sesclient.send_email(
             Destination={
                 'ToAddresses': [
@@ -86,6 +87,5 @@ def lambda_handler(event, context):
             },
             Source=recipient
         )
-    
-    
+
     return 'Successfully processed'
